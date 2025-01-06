3d0f1fb5c26cedbfd0c0a9894eeb55e90617371d
sudharshana <sudhubalu04@gmail.com>
2025-01-07 00:10:02 +0530

    navbar

--- app/page.js
+++ app/page.js
@@ -1,15 +1,6 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>My Portfolio</title>
-      </Head>
-      <main>
-        <h1>Welcome to My Portfolio!</h1>
-        <p>Hello, I'm [Your Name]. A [Your Profession].</p>
-      </main>
-    </div>
-  );
+    <main>
+    </main>
+  )
 }
---
+++ components/header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>LinkedIn</a>
+        <a>GitHub</a>
+        <a>LeetCode</a>
+        
+    </div>
+  )
+}
