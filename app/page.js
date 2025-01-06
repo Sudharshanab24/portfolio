d6b94e224b403a58bb2a2c62ba7a7d862865a092
sudharshana <sudhubalu04@gmail.com>
2025-01-06 19:36:26 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
+      <main>
+        <h1>Welcome to My Portfolio!</h1>
+        <p>Hello, I'm [Your Name]. A [Your Profession].</p>
+      </main>
+    </div>
+  );
+}
